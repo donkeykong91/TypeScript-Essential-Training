f0e01054c5dc1424e2d31f01f0571b81f34d611a
donkeykong91 <darylnator@gmail.com>
2018-09-23 01:32:17 -0700

    Added union types and type guarding

--- app.ts
+++ app.ts
@@ -1,6 +1,22 @@
-function totalLength(x: any[], y: string): number {
+function totalLength(x: (any[] | string), y: (string | any[])): number {
 
     let total: number = x.length + y.length;
+
+    x.slice(0);
+
+    if (x instanceof Array) {
+
+        x.push("abc");
+
+    }
+
+
+    if (typeof x === "string")
+    {
+
+        x.substr(1);
+
+    }
 
     return total;
 
