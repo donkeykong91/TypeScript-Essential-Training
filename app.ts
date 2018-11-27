225569fb48a990fa03aa55028c754a33e5653d36
Daryl <daryl@MOBOs-MacBook-Pro.local>
2018-11-27 05:24:29 -0800

    Added new interfaces and overloaded interfaces as well

--- app.ts
+++ app.ts
@@ -1,14 +1,62 @@
+interface Todo {
+
+    name: string;
+
+    completed?: boolean;
+
+}
+
 interface JQuery {
 
-    (selector: string): HTMLElement;
+    (selector: (string| any)): HTMLElement;
+
+    fn: any;
 
     version: number;
 
 }
 
-var $ = function (selector) {
+interface jQueryElement {
 
+    data(name: string): any;
+
+    data(name: string, data: any): jQueryElement;
 
 }
 
-$.version = 1.12;
+interface jQueryElement {
+
+    todo(): Todo;
+
+    todo(todo: Todo): jQueryElement;
+
+}
+
+var todo = 
+
+    {
+        name: 'Pick up drycleaning'
+    };
+
+$.fn.todo = function (todo?: Todo): Todo {
+
+    if (todo) {
+
+        $(this).data('todo', todo);
+
+    } else {
+
+        return $(this).data('todo');
+
+    }
+
+}
+
+var container = $('container');
+
+container.data('todo', todo);
+
+var savedTodo = container.data('todo');
+
+
+container.todo(todo);
