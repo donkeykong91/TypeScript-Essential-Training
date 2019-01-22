a4a1ffa1800a4e251d1cc78c29b86bab43ee44e2
Daryl <daryl@MOBOs-MacBook-Pro.local>
2019-01-22 06:33:26 -0800

    created generic classes

--- app.ts
+++ app.ts
@@ -169,10 +169,72 @@
 
 // }
 
-function clone<T> (value: T): T {
+// function clone<T> (value: T): T {
 
-    let serialized = JSON.stringify(value);
+//     let serialized = JSON.stringify(value);
 
-    return JSON.parse(serialized);
+//     return JSON.parse(serialized);
+
+// }
+
+// clone('Hello');
+
+// clone(123);
+
+// var todo: Todo = {
+
+//     id: 1,
+
+//     name: "Pick up drycleaning", 
+
+//     state: TodoState.Active
+
+// };
+
+// clone(todo);
+
+
+// clone({ name: "Jess" });
+
+var array: number[] = [1, 2, 3]
+
+var array2: Array<number> = [1, 2, 3]
+
+class KeyValuePair<TKey, TValue> {
+
+    constructor(
+
+        public key: TKey,
+
+        public value: TValue
+
+    ) {}
 
 }
+
+let pair1 = new KeyValuePair<number, string>(1, 'First');
+
+let pair2 = new KeyValuePair<string, Date>('Second', new Date(Date.now()));
+
+let pair3 = new KeyValuePair<number, string>(3, 'Third');
+
+
+class KeyValuePairPrinter<T, U> {
+
+    constructor(private pairs: KeyValuePair<T, U>[]) {}
+
+    print() {
+
+        for (let p of this.pairs) {
+
+            console.log(`${p.key}: ${p.value}`);
+
+        }
+
+    }   
+
+}
+
+var printer = new KeyValuePairPrinter([pair1, pair3]);
+
+printer.print();
