b6a4b0771e5bc6101901fc993a56f395f11c72ec
Daryl <daryl@MOBOs-MacBook-Pro.local>
2019-01-22 21:17:10 -0800

    generic constraints

--- app.ts
+++ app.ts
@@ -238,3 +238,22 @@ class KeyValuePairPrinter<T, U> {
 var printer = new KeyValuePairPrinter([pair1, pair3]);
 
 printer.print();
+
+interface IHaveALength {
+
+    length: number;
+
+}
+
+function totalLength<T extends IHaveALength>(x: T, y: T) {
+
+    var total: number = x.length + y.length;
+
+    return total;
+
+}
+
+class CustomArray<T> extends Array<T> {}
+
+var length = totalLength([1, 2, 3], new CustomArray<number>());
+
