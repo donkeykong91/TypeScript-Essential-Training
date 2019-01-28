5baf4bcb38b170430017a2055f0539e6f0255001
Daryl <daryl@MOBOs-MacBook-Pro.local>
2019-01-28 05:46:52 -0800

    commented out a lot of code

--- app.ts
+++ app.ts
@@ -196,64 +196,135 @@
 
 // clone({ name: "Jess" });
 
-var array: number[] = [1, 2, 3]
+// var array: number[] = [1, 2, 3]
 
-var array2: Array<number> = [1, 2, 3]
+// var array2: Array<number> = [1, 2, 3]
 
-class KeyValuePair<TKey, TValue> {
+// class KeyValuePair<TKey, TValue> {
 
-    constructor(
+//     constructor(
 
-        public key: TKey,
+//         public key: TKey,
 
-        public value: TValue
+//         public value: TValue
 
-    ) {}
+//     ) {}
 
-}
+// }
 
-let pair1 = new KeyValuePair<number, string>(1, 'First');
+// let pair1 = new KeyValuePair<number, string>(1, 'First');
 
-let pair2 = new KeyValuePair<string, Date>('Second', new Date(Date.now()));
+// let pair2 = new KeyValuePair<string, Date>('Second', new Date(Date.now()));
 
-let pair3 = new KeyValuePair<number, string>(3, 'Third');
+// let pair3 = new KeyValuePair<number, string>(3, 'Third');
 
 
-class KeyValuePairPrinter<T, U> {
+// class KeyValuePairPrinter<T, U> {
 
-    constructor(private pairs: KeyValuePair<T, U>[]) {}
+//     constructor(private pairs: KeyValuePair<T, U>[]) {}
 
-    print() {
+//     print() {
 
-        for (let p of this.pairs) {
+//         for (let p of this.pairs) {
 
-            console.log(`${p.key}: ${p.value}`);
+//             console.log(`${p.key}: ${p.value}`);
 
-        }
+//         }
 
-    }   
+//     }   
 
-}
+// }
 
-var printer = new KeyValuePairPrinter([pair1, pair3]);
+// var printer = new KeyValuePairPrinter([pair1, pair3]);
 
-printer.print();
+// printer.print();
 
-interface IHaveALength {
+// interface IHaveALength {
 
-    length: number;
+//     length: number;
 
-}
+// }
 
-function totalLength<T extends IHaveALength>(x: T, y: T) {
+// function totalLength<T extends IHaveALength>(x: T, y: T) {
 
-    var total: number = x.length + y.length;
+//     var total: number = x.length + y.length;
 
-    return total;
+//     return total;
 
-}
+// }
 
-class CustomArray<T> extends Array<T> {}
+// class CustomArray<T> extends Array<T> {}
 
-var length = totalLength([1, 2, 3], new CustomArray<number>());
+// var length = totalLength([1, 2, 3], new CustomArray<number>());
 
+// namespace TodoApp.Model {
+
+//     export interface Todo {
+
+//         name: string;
+
+//         state: TodoState;
+
+//     }
+
+// }
+
+// namespace TodoApp.Model {
+
+//     export enum TodoState {
+
+//         New = 1,
+
+//         Active,
+
+//         Complete,
+
+//         Deleted
+
+//     }
+
+// }
+
+// namespace DataAccess {
+
+//     import Model = TodoApp.Model;
+
+//     import Todo = Model.Todo;
+
+//     export interface ITodoService {
+
+//         add(todo: Todo): Todo;
+
+//         delete(todoId: number): void;
+
+//         getAll(): Todo[];
+
+//         getById(todoId: number): Todo;
+
+//     }
+
+// }
+
+var jQuery = {
+
+    version: 1.19,
+
+    fn: {}
+
+};
+
+(function defineType($) {
+
+    if ( $.version < 1.15 ) {
+
+        throw "Plugin requires jQuery version 1.15+"
+
+    }
+
+    $.fn.myPlugin = function() {
+
+        // my plugin code
+
+    }
+
+})(jQuery)
