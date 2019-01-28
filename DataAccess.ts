40ad86fdd650d1ee45675efa9bf817511ad2e6da
Daryl <daryl@MOBOs-MacBook-Pro.local>
2019-01-28 05:46:12 -0800

    added these files to demonstrate namespace keyword

---
+++ DataAccess.ts
@@ -0,0 +1,77 @@
+namespace DataAccess {
+
+    import Model = TodoApp.Model;
+
+    import Todo = Model.Todo;
+
+
+    let _lastId: number = 0;
+
+
+    export interface ITodoService {
+
+        add(todo: Todo): Todo;
+
+        delete(todoId: number): void;
+
+        getAll(): Todo[];
+
+        getById(todoId: number): Todo;
+
+    }
+
+    function generateTodoId() {
+    
+        return _lastId += 1;
+
+    }
+
+
+    class TodoService implements ITodoService{
+    
+        constructor(private todos: Todo[]) {}
+    
+        add(todo: Todo): Todo {
+    
+            todo.id = generateTodoId();
+    
+            this.todos.push(todo);
+    
+            return todo;
+    
+        }
+    
+        getAll() {
+    
+            var clone = JSON.stringify(this.todos);
+    
+            return JSON.parse(clone);
+    
+        }
+    
+        getById(todoId: number): Todo {
+    
+            var filtered = this.todos.filter( x => x.id == todoId);
+    
+            if (filtered.length) {
+    
+                return filtered[0];
+    
+            }
+    
+            return null;
+    
+        }
+    
+        delete(todoId: number): void {
+    
+            var toDelete = this.getById(todoId);
+    
+            var deletedIndex = this.todos.indexOf(toDelete);
+    
+            this.todos.splice(deletedIndex, 1);
+    
+        }
+    
+    }
+}
