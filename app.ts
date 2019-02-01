910f6163db4c5f013ee931defa62e057fc478ada
Daryl <daryl@MOBOs-MacBook-Pro.local>
2019-02-01 05:56:46 -0800

    added code to demonstrate the loading of external modules

--- app.ts
+++ app.ts
@@ -328,3 +328,34 @@
 //     }
 
 // })(jQuery)
+
+import { Todo as TodoTask, TodoState } from './model';
+
+import { TodoService } from './DataAccess';
+
+
+{let service = new TodoService([]);
+
+    service.add(
+
+        {
+            id: 1,
+
+            name: 'Pick up drycleaning',
+
+            state: TodoState.New
+        }
+
+    );
+
+    {let todos = service.getAll();
+
+        todos.forEach(function(todo) {
+
+            console.log(`${todo.name} [${TodoState[todo.state]}]`);
+
+        });
+
+    }
+
+}
